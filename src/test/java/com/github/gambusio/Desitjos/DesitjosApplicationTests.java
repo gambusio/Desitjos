7125893d2ad4c647ff1423df772c96939b9267f0
Felipe González Hernández <gambusio@gmail.com>
2022-12-04 17:28:57 +0000

    Initial commit

---
+++ src/test/java/com/github/gambusio/Desitjos/DesitjosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.gambusio.Desitjos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesitjosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
