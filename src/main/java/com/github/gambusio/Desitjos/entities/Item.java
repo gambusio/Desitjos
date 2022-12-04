5990005f2fc36a66c753ccbaf04260424689c0d2
Felipe González Hernández <gambusio@gmail.com>
2022-12-04 17:39:35 +0000

    Added Item.java and ItemType.java

---
+++ src/main/java/com/github/gambusio/Desitjos/entities/Item.java
@@ -0,0 +1,12 @@
+package com.github.gambusio.Desitjos.entities;
+
+import java.math.BigDecimal;
+
+public class Item {
+    private String sName ="";
+    private String sDescription = "";
+    private String sUrl = "";
+    private String sPicUrl = "";
+    private ItemType itemType;
+    private BigDecimal price;
+}
---
+++ src/main/java/com/github/gambusio/Desitjos/entities/ItemType.java
@@ -0,0 +1,5 @@
+package com.github.gambusio.Desitjos.entities;
+
+public enum ItemType {
+    ECOMMERCE, AUCTION, PRIVATE_SALE
+}
